7101968ee33a2810fa572b4722f2fc0dd8b7a15b
Otssn <88810729+Otssn@users.noreply.github.com>
2021-10-11 14:59:12 -0500

    Add Usertables

---
+++ Vehicles/Vehicles.API/Data/Entities/User.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles.commons.Enums;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class User : IdentityUser
+    {
+        [Display(Name = "Nombres")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatiorio.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Apellidos")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatiorio.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Tipo de documento")]
+        [Required(ErrorMessage = "El campo {0} es obligatiorio.")]
+        public DocumentTypes DocumentType { get; set; }
+
+        [Display(Name = "Documento")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatiorio.")]
+        public string Document { get; set; }
+
+        [Display(Name = "Dirección")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatiorio.")]
+        public string Addres { get; set; }
+
+        [Display(Name = "Foto")]
+        public Guid ImageId { get; set; }
+
+        //TODO: Fix the images path
+        [Display(Name = "Foto")]
+        public string ImageFullPath => ImageId == Guid.Empty
+            ? $"https://https://localhost:44371/images/noimage.png"
+            : $"https://vehiclessalazar.blob.core.windows.net/users/{ImageId}";
+
+        [Display(Name = "Tipo de usuario")]
+        public UserType userType { get; set; }
+
+        [Display(Name = "Usuario")]
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
--- Vehicles/Vehicles.API/Data/SeedDb.cs
+++ Vehicles/Vehicles.API/Data/SeedDb.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Vehicles.API.Data.Entities;
+using Vehicles.API.Helpers;
+using Vehicles.commons.Enums;
 
 namespace Vehicles.API.Data
 {
     public class SeedDb
     {
         private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, IUserHelper userHelper)
         {
             _context = context;
+            _userHelper = userHelper;
         }
 
         public async Task SeedAsync()
@@ -22,6 +26,38 @@ namespace Vehicles.API.Data
             await CheckBrandsAsync();
             await CheckDocumentTypesAsync();
             await CheckProceduresAsync();
+            await CheckRikesAsync();
+            await CheckUserAsync("1010", "Camilo", "Torres", "camilo@yopmail.com" , "3113123123" , "calle 123", UserType.Admin);
+            await CheckUserAsync("2020", "Esteban", "Pabon", "esteban@yopmail.com", "3113123123", "calle 123", UserType.User);
+            await CheckUserAsync("3030", "Sara", "Davila", "saris@yopmail.com", "3113123123", "calle 123", UserType.User);
+        }
+
+        private async Task CheckUserAsync(string document, string fisrtName, string lastName, string email, string phoneNumber, string addres, UserType userType)
+        {
+            User user = await _userHelper.GetUserAsync(email);
+            if(user == null)
+            {
+                user = new User
+                {
+                    Addres = addres,
+                    Document = document,
+                    DocumentType = _context.DocumentTypes.FirstOrDefault(x => x.Description == "Cédula"),
+                    Email = email,
+                    FirstName = fisrtName,
+                    LastName = lastName,
+                    PhoneNumber = phoneNumber,
+                    UserName = email,
+                    userType = userType
+                };
+                await _userHelper.AddUserAsync(user, "123456");
+                await _userHelper.AddUserToRoleAsync(user, userType.ToString());
+            }
+        }
+
+        private async Task CheckRikesAsync()
+        {
+            await _userHelper.CheckRoleAsync(UserType.Admin.ToString());
+            await _userHelper.CheckRoleAsync(UserType.User.ToString());
         }
 
         private async Task CheckProceduresAsync()
